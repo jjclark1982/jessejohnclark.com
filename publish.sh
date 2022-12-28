c1a4a9a8b14678965a877e95999a993942a78cc0
Jesse Clark <jjclark1982@gmail.com>
2022-12-28 21:21:36 +1100

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 SOURCE_BRANCH=$(git rev-parse HEAD)
 SOURCE_PARENT=$(git rev-parse -q --verify $SOURCE_BRANCH)
 SOURCE_MESSAGE=$(git log -1 --pretty=%B)
-PUBLISH_BRANCH=master
+PUBLISH_BRANCH=gh-pages
 PUBLISH_PARENT=$(git rev-parse -q --verify $PUBLISH_BRANCH)
 
 if [ "$SOURCE_BRANCH" = "$PUBLISH_BRANCH" ]; then
@@ -13,18 +13,24 @@ fi
 
 rm -rf build
 git clone . build
-(cd build; git checkout $PUBLISH_BRANCH; rm -r *)
+(
+    cd build
+    git checkout $PUBLISH_BRANCH
+    rm -r *
+)
 
 # build using the hardcoded config: no cleaning or watching
-gulp build
+npm run build
 
-cd build
-git add --all .
-git update-ref refs/heads/$PUBLISH_BRANCH $(
-    git commit-tree \
-        ${PUBLISH_PARENT:+-p $PUBLISH_PARENT} \
-        -p $SOURCE_PARENT \
-        -m "$SOURCE_MESSAGE (built by $0)" \
-        $(git write-tree)
+(
+    cd build
+    git add --all .
+    git update-ref refs/heads/$PUBLISH_BRANCH $(
+        git commit-tree \
+            ${PUBLISH_PARENT:+-p $PUBLISH_PARENT} \
+            -p $SOURCE_PARENT \
+            -m "$SOURCE_MESSAGE (built by $0)" \
+            $(git write-tree)
+    )
+    git push origin $PUBLISH_BRANCH
 )
-git push origin $PUBLISH_BRANCH
